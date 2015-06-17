1e45d06b979df75b43ece51969c3ee593e1d3dc0
melstone <andrei.meleshin@gmail.com>
2015-06-17 20:08:33 +0300

    Страница about добавлена!

---
+++ about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+	<title>О проекте - CityMetrix</title>
+
+	<!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <meta name="format-detection" content="telephone=no">
+
+	<link rel="shortcut icon" href="/favicon.ico">
+
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+
+	<link rel="stylesheet" href="/css/main.css">
+
+	<script src="/js/jquery-1.11.3.min.js"></script>
+	<script src="/js/main.js"></script>
+
+	<!--[if lt IE 9]>
+		<script src="/js/css3-mediaqueries.js"></script>
+		<script src="/js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+	
+	<div class="canvas">
+		<div class="header">
+			<div class="container">
+				<a href="/" class="logo"></a>
+
+				<div class="main-navigation">
+					<ul>
+						<li class="auto"><a href="/category.php">Транспорт</a></li>
+						<li class="house"><a href="/category.php">Дом</a></li>
+						<li class="shopping"><a href="/category.php">Шоппинг</a></li>
+						<li class="neighbors"><a href="/category.php">Соседи</a></li>
+						<li class="culture"><a href="/category.php">Досуг</a></li>
+						<li class="work"><a href="/category.php">Работа</a></li>
+						<li class="health"><a href="/category.php">Здоровье</a></li>
+						<li class="education"><a href="/category.php">Образование</a></li>
+						<li class="security"><a href="/category.php">Безопасность</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- / .header -->
+
+		<div class="page container">
+			<div class="page-title">
+				<h1>О проекте</h1>
+			</div>
+			
+			<div class="small-wrap">
+				<p>
+					Городской интернет-сайт, который рассказывает о культурнои и общественнои жизни, развлечениях, услугах, еде и людях в Москве, Петербурге и других мегаполисах. Мы пишем о комфортном городе, широко трактуя это понятие: удобство жизни в мегаполисе зависит одновременно и от глобальных градостроительных инициатив, и от количества мест, где можно выпить хороший кофе. Поэтому на страницах The Village интервью с чиновниками соседствуют с обзорами кафе или баров. В формировании материалов, наряду с редакциеи, принять участие может любой зарегистрированный пользователь. Обсуждение городскоижизни также активно идет в разделе «Комьюнити».
+				</p>
+			</div>
+		</div>
+		<!-- / .page -->
+	</div>
+		
+
+	<div class="footer">
+		<div class="container">
+			<a href="/" class="footer-logo"></a>
+			<div class="footer-copyright">
+				© Citymetrix
+			</div>
+			<div class="footer-navigation">
+				<ul>
+					<li><a href="/contacts.php">Контакты</a></li>
+					<li><a href="/about.php">О проекте</a></li>
+					<li><a class="link-underline" href="/lp">Мобильное приложение</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- / .footer -->
+	
+</body>
+</html>
