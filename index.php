007e3fe713d7b7daa1665f3ae5b0372610c023a1
melstone <andrei.meleshin@gmail.com>
2015-06-17 20:03:57 +0300

    Новый файлик index добавил :)

---
+++ index.php
@@ -0,0 +1,406 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+	<title>Главная - CityMetrix</title>
+
+	<!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <meta name="format-detection" content="telephone=no">
+
+	<link rel="shortcut icon" href="/favicon.ico">
+
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+
+	<link rel="stylesheet" href="/css/main.css">
+
+	<script src="/js/jquery-1.11.3.min.js"></script>
+	<script src="/js/main.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js"></script>
+
+	<!--[if lt IE 9]>
+		<script src="/js/css3-mediaqueries.js"></script>
+		<script src="/js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+	
+	<div class="canvas">
+		<div class="header">
+			<div class="container">
+				<a href="/" class="logo"></a>
+
+				<div class="main-navigation">
+					<ul>
+						<li class="auto"><a href="/category.php">Транспорт</a></li>
+						<li class="house"><a href="/category.php">Дом</a></li>
+						<li class="shopping"><a href="/category.php">Шоппинг</a></li>
+						<li class="neighbors"><a href="/category.php">Соседи</a></li>
+						<li class="culture"><a href="/category.php">Досуг</a></li>
+						<li class="work"><a href="/category.php">Работа</a></li>
+						<li class="health"><a href="/category.php">Здоровье</a></li>
+						<li class="education"><a href="/category.php">Образование</a></li>
+						<li class="security"><a href="/category.php">Безопасность</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- / .header -->
+
+		<div class="container">
+			<a href="#" class="b-analytics-link">
+				Городская аналитика: Москва в цифрах
+			</a>
+		</div>
+
+		<div class="page container">
+			<div class="b-home-grid-events">
+				<div class="home-grid-wrapper">
+					<div class="home-grid">
+						<div class="home-grid-col">
+							<div class="home-grid-item" style="padding-bottom: 58%">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_001.jpg);"></div>
+								<span class="meta">
+									<span class="category education">Образование</span>
+									<span class="title">
+										Рейтинг школ
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+
+							<div class="home-grid-item" style="padding-bottom: 58%">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_002.jpg);"></div>
+								<span class="meta">
+									<span class="category auto">Транспорт</span>
+									<span class="title">
+										СТАТИСТИКА УГОНОВ В МОСКВЕ <br>
+										ЗА 1 КВАРТАЛ 2015 ГОДА
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+
+							<div class="home-grid-row">
+								<div class="home-grid-col">
+									<div class="home-grid-item" style="padding-bottom: 85%">
+										<a href="article.php"></a>
+										<div class="image" style="background-image: url(/storage/home_grid_item_image_003.jpg);"></div>
+										<span class="meta">
+											<span class="category auto">Транспорт</span>
+											<span class="title">
+												СКОРОСТЬ НА ОСНОВНЫХ <br>
+												ДОРОГАХ МОСКВЫ
+											</span>
+											<span class="date">10 июня</span>
+										</span>
+									</div>
+									
+								</div>
+								<div class="home-grid-col">
+									<div class="home-grid-item" style="padding-bottom: 85%">
+										<a href="article.php"></a>
+										<div class="image" style="background-image: url(/storage/home_grid_item_image_004.jpg);"></div>
+										<span class="meta">
+											<span class="category work">Работа</span>
+											<span class="title">
+												СРЕДНЯЯ ЗАРПЛАТА ПО <br>
+												ТАГАНСКОМУ РАЙОНУ
+											</span>
+											<span class="date">10 июня</span>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="home-grid-col">
+							<div class="home-grid-row">
+								<div class="home-grid-col special">
+									<div class="home-grid-item home-grid-item--digit-of-day">
+										<div class="image"></div>
+										<div class="inner">
+											<div class="title">
+												Цифра дня
+											</div>
+											<div class="value">
+												90 <span>км/ч</span>
+											</div>
+											<div class="caption">
+												средняя <br>
+												скорость на <br>
+												МКАДе
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="home-grid-col special">
+									<div class="home-grid-item home-grid-item--about">
+										<a href="/about.php"></a>
+										<div class="image"></div>
+										<div class="inner">
+											<div class="icon">
+											</div>
+											<div class="title">
+												О проекте
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="home-grid-col special">
+									<div class="home-grid-item home-grid-item--polls">
+										<a href="#"></a>
+										<div class="image"></div>
+										<div class="inner">
+											<div class="icon">
+											</div>
+											<div class="title">
+												Опросы
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="home-grid-col special">
+									<div class="home-grid-item home-grid-item--facts">
+										<div class="image"></div>
+										<div class="inner">
+											<div class="title">
+												Факты <br>
+												о городе
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="home-grid-item small">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_005.jpg);"></div>
+								<span class="meta">
+									<span class="category auto">Транспорт</span>
+									<span class="title">
+										ЗАГРУЗКА СТАНЦИЙ МОСКОВСКОГО <br>
+										МЕТРОПОЛИТЕНА ЗА МАЙ 2015
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+
+							<div class="home-grid-row">
+								<div class="home-grid-col">
+									<div class="home-grid-item">
+										<a href="article.php"></a>
+										<div class="image" style="background-image: url(/storage/home_grid_item_image_006.jpg);"></div>
+										<span class="meta">
+											<span class="category health">Здоровье</span>
+											<span class="title">
+												МЕД. ПОДДЕРЖКА <br>
+												ЖИТЕЛЕЙ. СТАТИСТИКА
+											</span>
+											<span class="date">10 июня</span>
+										</span>
+									</div>
+								</div>
+								<div class="home-grid-col">
+									<div class="home-grid-item">
+										<a href="article.php"></a>
+										<div class="image" style="background-image: url(/storage/home_grid_item_image_007.jpg);"></div>
+										<span class="meta">
+											<span class="category neighbors">Соседи</span>
+											<span class="title">
+												НАЦИОНАЛЬНЫЙ СОСТАВ <br>
+												МОСКВЫ 2015
+											</span>
+											<span class="date">10 июня</span>
+										</span>
+									</div>
+								</div>
+							</div>
+
+							<div class="home-grid-item">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_008.jpg);"></div>
+								<span class="meta">
+									<span class="category house">Дом</span>
+									<span class="title">
+										СРАВНИТЕЛЬНЫЙ АНАЛИЗ ПО СТОИМОСТИ <br>
+										АРЕНДЫ КВАРТИРЫ В МОСКВЕ. ОБЗОР ПО РАЙОНАМ
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+						</div>
+					</div>
+					<!-- / .home-grid -->
+
+					<div class="home-grid">
+						<div class="home-grid-col">
+							<div class="home-grid-item">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_001.jpg);"></div>
+								<span class="meta">
+									<span class="category education">Образование</span>
+									<span class="title">
+										Рейтинг школ
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+
+							<div class="home-grid-item">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_002.jpg);"></div>
+								<span class="meta">
+									<span class="category auto">Транспорт</span>
+									<span class="title">
+										СТАТИСТИКА УГОНОВ В МОСКВЕ <br>
+										ЗА 1 КВАРТАЛ 2015 ГОДА
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+
+							<div class="home-grid-row">
+								<div class="home-grid-col">
+									<div class="home-grid-item">
+										<a href="article.php"></a>
+										<div class="image" style="background-image: url(/storage/home_grid_item_image_003.jpg);"></div>
+										<span class="meta">
+											<span class="category auto">Транспорт</span>
+											<span class="title">
+												СКОРОСТЬ НА ОСНОВНЫХ <br>
+												ДОРОГАХ МОСКВЫ
+											</span>
+											<span class="date">10 июня</span>
+										</span>
+									</div>
+									
+								</div>
+								<div class="home-grid-col">
+									<div class="home-grid-item">
+										<a href="article.php"></a>
+										<div class="image" style="background-image: url(/storage/home_grid_item_image_004.jpg);"></div>
+										<span class="meta">
+											<span class="category work">Работа</span>
+											<span class="title">
+												СРЕДНЯЯ ЗАРПЛАТА ПО <br>
+												ТАГАНСКОМУ РАЙОНУ
+											</span>
+											<span class="date">10 июня</span>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="home-grid-col">
+							<div class="home-grid-item">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_008.jpg);"></div>
+								<span class="meta">
+									<span class="category house">Дом</span>
+									<span class="title">
+										СРАВНИТЕЛЬНЫЙ АНАЛИЗ ПО СТОИМОСТИ <br>
+										АРЕНДЫ КВАРТИРЫ В МОСКВЕ. ОБЗОР ПО РАЙОНАМ
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+
+							<div class="home-grid-item small">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_005.jpg);"></div>
+								<span class="meta">
+									<span class="category auto">Транспорт</span>
+									<span class="title">
+										ЗАГРУЗКА СТАНЦИЙ МОСКОВСКОГО <br>
+										МЕТРОПОЛИТЕНА ЗА МАЙ 2015
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+
+							<div class="home-grid-row">
+								<div class="home-grid-col">
+									<div class="home-grid-item">
+										<a href="article.php"></a>
+										<div class="image" style="background-image: url(/storage/home_grid_item_image_006.jpg);"></div>
+										<span class="meta">
+											<span class="category health">Здоровье</span>
+											<span class="title">
+												МЕД. ПОДДЕРЖКА <br>
+												ЖИТЕЛЕЙ. СТАТИСТИКА
+											</span>
+											<span class="date">10 июня</span>
+										</span>
+									</div>
+								</div>
+								<div class="home-grid-col">
+									<div class="home-grid-item">
+										<a href="article.php"></a>
+										<div class="image" style="background-image: url(/storage/home_grid_item_image_007.jpg);"></div>
+										<span class="meta">
+											<span class="category neighbors">Соседи</span>
+											<span class="title">
+												НАЦИОНАЛЬНЫЙ СОСТАВ <br>
+												МОСКВЫ 2015
+											</span>
+											<span class="date">10 июня</span>
+										</span>
+									</div>
+								</div>
+							</div>
+
+							<div class="home-grid-item">
+								<a href="article.php"></a>
+								<div class="image" style="background-image: url(/storage/home_grid_item_image_008.jpg);"></div>
+								<span class="meta">
+									<span class="category house">Дом</span>
+									<span class="title">
+										СРАВНИТЕЛЬНЫЙ АНАЛИЗ ПО СТОИМОСТИ <br>
+										АРЕНДЫ КВАРТИРЫ В МОСКВЕ. ОБЗОР ПО РАЙОНАМ
+									</span>
+									<span class="date">10 июня</span>
+								</span>
+							</div>
+						</div>
+					</div>
+					<!-- / .home-grid -->
+				</div>
+
+				<div class="button lined" data-action="show-more-events">Показать ещё</div>
+			</div>
+			<!-- / .b-home-grid-events -->
+
+			<div class="b-find-error">
+				если вы заметили какую-то неточность сообщите нам на <a href="mailto:info@citymetrix.ru">info@citymetrix.ru</a>
+			</div>
+
+		</div>
+		<!-- / .page -->
+	</div>
+		
+
+	<div class="footer">
+		<div class="container">
+			<a href="/" class="footer-logo"></a>
+			<div class="footer-copyright">
+				© Citymetrix
+			</div>
+			<div class="footer-navigation">
+				<ul>
+					<li><a href="/contacts.php">Контакты</a></li>
+					<li><a href="/about.php">О проекте</a></li>
+					<li><a class="link-underline" href="/lp">Мобильное приложение</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- / .footer -->
+	
+</body>
+</html>
